e98b254922b8ed228222529d0e181d4caedfe3da
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-03-03 13:46:35 +0530

    Time: 5 ms (85.39%) | Memory: 68.3 MB (24.11%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,29 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> less = new ArrayList<>();
-        int pCount = 0;
-        for(int num : nums){
-            if(num < pivot) {
-                less.add(num);
-            }
-            if(num == pivot) pCount++;
-        }
-        for(int i = 0; i < pCount; i++) {
-            less.add(pivot);
-        }
-        ArrayList<Integer> greater = new ArrayList<>();
-        for(int num : nums) {
-            if(num > pivot) {
-                greater.add(num);
+        int n = nums.length;
+        int m = 0;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < pivot) {
+                ans[m++] = nums[i];
             }
         }
-        int[] ans = new int[nums.length];
-        int ind = 0;
-        for(int el : less) {
-            ans[ind++] = el;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == pivot) {
+                ans[m++] = nums[i];
+            }
         }
-        for(int el : greater) {
-            ans[ind++] = el;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > pivot){
+                ans[m++] = nums[i];
+            }
         }
         return ans;
     }
