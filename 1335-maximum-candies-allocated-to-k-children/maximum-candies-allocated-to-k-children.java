59b1296679ebeb7e89682709bdb846885533dec0
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-03-14 16:48:04 +0530

    Time: 32 ms (33.87%) | Memory: 63.5 MB (13.98%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        // range to identify possible distribution
+        int start = 1, end = 10_000_000;
+        int ans = 0;
+        // search possible distribution using BS
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            // count possible count of children get candies
+            long possiChildCount = 0;
+            // add posible child 
+            for(int c : candies) {
+                possiChildCount += c / mid;
+            }
+            // check whether the distribution feed possible children
+            if(possiChildCount >= k) {
+                // possible ans
+                ans = mid;
+                start = mid + 1;
+            } else {
+                // else start search in upper bound
+                end = mid - 1;
+            }
+        }
+        // return max possible distribution
+        return ans;
+    }
+}
