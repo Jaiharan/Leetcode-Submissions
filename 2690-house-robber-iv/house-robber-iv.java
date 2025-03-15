bf1b8e3e802e5bd16760db49a65faf2b7306689e
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-03-15 14:49:06 +0530

    Time: 17 ms (97.01%) | Memory: 55.9 MB (79.10%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,12 +1,10 @@
 class Solution {
     public int minCapability(int[] nums, int k) {
         int min = Integer.MAX_VALUE, max = 0;
-
         for(int num : nums) {
             if(num > max) max = num;
             if(num < min) min = num;
         }
-
         while(min < max) {
             int mid = min + (max - min) / 2;
             if(checkMinKElements(nums, k, mid)) {
@@ -19,16 +17,16 @@ class Solution {
     }
 
     private boolean checkMinKElements(int[] nums, int k, int mid) {
-        int cnt = 0;
+        int houseCnt = 0;
         int i = 0;
         while(i < nums.length) {
             if(nums[i] <= mid) {
-                cnt++;
+                houseCnt++;
                 i += 2;
             } else {
                 i++;
             }
         }
-        return cnt >= k;
+        return houseCnt >= k;
     }
 }
