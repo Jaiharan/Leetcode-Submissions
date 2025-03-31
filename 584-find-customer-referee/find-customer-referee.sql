130b1d76f9196ea1c91f179513b328ca2f815c79
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-03-31 15:10:36 +0530

    Time: 478 ms (92.74%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-SELECT name from Customer WHERE referee_id != 2 or referee_id is null;
--- SELECT name FROM Customer WHERE COALESCE(referee_id, 0) <> 2;
+-- Simple method
+-- SELECT name from Customer WHERE referee_id != 2 or referee_id is null;
+
+-- USING COALESCE, this initialize field with default value before execution
+SELECT name FROM Customer WHERE COALESCE(referee_id, 0) != 2;
