0b27a4b7738112fcf6791f4fa72843c2c45eac7c
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-03-31 15:10:51 +0530

    Time: 475 ms (93.39%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -3,4 +3,4 @@
 -- SELECT name from Customer WHERE referee_id != 2 or referee_id is null;
 
 -- USING COALESCE, this initialize field with default value before execution
-SELECT name FROM Customer WHERE COALESCE(referee_id, 0) != 2;
+SELECT name FROM Customer WHERE COALESCE(referee_id, 0) <> 2;
