ddc7e9592f569ce04700b7e47f5c2076b15afbea
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-04-24 22:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (22.20%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,20 +15,39 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
-        q.offer(root);
-        while(!q.isEmpty()) {
-            int levelNum = q.size();
-            List<Integer> subList = new ArrayList<>();
-            for(int i = 0; i < levelNum; i++) {
-                if(q.peek().left != null) q.offer(q.peek().left);
-                if(q.peek().right != null) q.offer(q.peek().right);
-                subList.add(q.poll().val);
-            }
-            ans.add(subList);
+        // // level order traversal
+        // Queue<TreeNode> q = new LinkedList<>();
+        // List<List<Integer>> ans = new ArrayList<>();
+        // if(root == null) return ans;
+        // q.offer(root);
+        // while(!q.isEmpty()) {
+        //     int levelNum = q.size();
+        //     List<Integer> subList = new ArrayList<>();
+        //     for(int i = 0; i < levelNum; i++) {
+        //         if(q.peek().left != null) q.offer(q.peek().left);
+        //         if(q.peek().right != null) q.offer(q.peek().right);
+        //         subList.add(q.poll().val);
+        //     }
+        //     ans.add(subList);
+        // }
+        // return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        bfs(root, res, 0);
+        return res;
+    }
+
+    private void bfs(TreeNode root, List<List<Integer>> res, int layer) {
+        if(root == null) {
+            return;
         }
-        return ans;
+        if(res.size() == layer) {
+            List<Integer> l = new ArrayList<>();
+            l.add(root.val);
+            res.add(l);
+        } else {
+            res.get(layer).add(root.val);
+        }
+        bfs(root.left, res ,layer + 1);
+        bfs(root.right, res ,layer + 1);
     }
 }
