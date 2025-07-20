4a43b02fcc6bca3c60e01e36aa08547c4166af22
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-07-21 00:32:00 +0530

    Time: 397 ms (83.98%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select w1.id from Weather w1 join Weather w2 on DateDiff(w1.recordDate, w2.recordDate) = 1 where w1.temperature > w2.temperature; 
+-- select w1.id from Weather w1 join Weather w2 on DateDiff(w1.recordDate, w2.recordDate) = 1 where w1.temperature > w2.temperature; 
+
+with previousWeatherData as (
+    select id, recordDate, temperature, lag(temperature, 1) over (order by recordDate) as previousTemperature, lag(recordDate, 1) over (order by recordDate) as previousRecordDate from Weather
+)
+
+select id from previousWeatherData where temperature > previousTemperature and recordDate = DATE_ADD(previousRecordDate, interval 1 day);
