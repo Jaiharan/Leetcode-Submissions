9ddceb9da4a6b2134309b83851c8721d09ddbb23
Jaiharan S <136414923+Jaiharan@users.noreply.github.com>
2025-08-31 17:34:05 +0530

    Time: 56 ms (91.09%) | Memory: 43.1 MB (28.58%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,23 +1,21 @@
 class Solution {
     public int beautySum(String s) {
-        int cnt = 0;
-        for(int i = 0; i < s.length(); i++) {
-            for(int j = i + 1; j < s.length(); j++) {
-                int beautyCnt = findBeauty(s.substring(i, j+1));
-                if(beautyCnt > 0) cnt += beautyCnt;
+         int n = s.length();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++;
+                int maxFreq = 0, minFreq = Integer.MAX_VALUE;
+                for (int f : freq) {
+                    if (f > 0) {
+                        maxFreq = Math.max(maxFreq, f);
+                        minFreq = Math.min(minFreq, f);
+                    }
+                }
+                res += (maxFreq - minFreq);
             }
         }
-        return cnt;
-    }
-
-    public int findBeauty(String s) {
-        int[] freq = new int[26];
-        for(char  ch : s.toCharArray()) freq[ch - 'a']++;
-        int low = Integer.MAX_VALUE, high = 0;
-        for(int f : freq) {
-            if(f > high) high = f;
-            if(f < low && f != 0) low = f;
-        }
-        return high - low;
+        return res;
     }
 }
